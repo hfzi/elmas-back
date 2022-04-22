445e06e4dbd1d52d393ac7fa2cacd4418c452675
HIFZI FURKAN KUTAY <furkankutay06@gmail.com>
2022-04-22 21:59:26 +0300

    filter güncellendi

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require("cors");
 
 const url = `https://blendermarket.com/creators/elmas?page=`;
+const link = `https://blendermarket.com/products/`
 
 app.use(cors());
 
@@ -13,6 +14,26 @@ app.get("/", function (req, res) {
   res.send("sa")
 });
 
+// ürün sayası
+app.get("/:page", function (req, res) {
+  console.log("link", link + req.params.page)
+  axios(link + req.params.page).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+    $(".product-content.quarantine.js-product-content-show").each(function () {
+      const url = $(this).find("p").text()
+      const img = $(this).find("img").eq(1).attr("src")
+      articles.push({
+        img,
+        url
+      });
+    });
+    res.json(articles);
+  });
+});
+
+// sayfa sayfası
 app.get("/product/:page", function (req, res) {
   axios(url + req.params.page).then((response) => {
     const html = response.data;
@@ -43,9 +64,11 @@ app.get("/page/:from-:to", async function (req, res) {
         let html = response.data;
         let $ = cheerio.load(html);
         $(".col-12.col-md-6.col-lg-3.mb-4").each(function () {
-          let url = $(this).find("a").attr("href").split("/products");
+          let title = $(this).find("h5").text().split(" (Rigged Car)").map(x => x);
+          let name = title[0] + title[1]
+          let link = $(this).find("a").attr("href").split("/products").map(x => x);
+          let url = link[0] + link[1]
           let img = $(this).find("img").attr("src").substring(0, 101);
-          let name = $(this).find("h5").text().split(" (Rigged Car)");
           let data = { name, url, img };
           articles.push(data);
         });
